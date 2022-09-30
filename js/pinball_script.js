a9d043e3c731998cf927ab741ba309b5bfec11de
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-09-30 14:07:57 +0900

    見やすく整えた。
    インデントをきれいに並べた。

--- js/pinball_script.js
+++ js/pinball_script.js
@@ -55,7 +55,6 @@ async function loadMobileNetFeatureModel() {
 // Call the function immediately to start loading.
 loadMobileNetFeatureModel();
 
-
 let model = tf.sequential();
 
 model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
@@ -106,7 +105,7 @@ function enableCam() {
 /**
  * Handle Data Gather for button mouseup/mousedown.
  **/
- function gatherDataForClass() {
+function gatherDataForClass() {
   let classNumber = parseInt(this.getAttribute('data-1hot'));
   gatherDataState = (gatherDataState === STOP_DATA_GATHER) ? classNumber : STOP_DATA_GATHER;
   dataGatherLoop();
@@ -116,8 +115,7 @@ function dataGatherLoop() {
   if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
     let imageFeatures = tf.tidy(function() {
       let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
-      let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT,
-          MOBILE_NET_INPUT_WIDTH], true);
+      let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT,MOBILE_NET_INPUT_WIDTH], true);
       let normalizedTensorFrame = resizedTensorFrame.div(255);
       return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
     });
@@ -148,7 +146,7 @@ async function trainAndPredict() {
   let inputsAsTensor = tf.stack(trainingDataInputs);
 
   let results = await model.fit(inputsAsTensor, oneHotOutputs, {shuffle: true, batchSize: 5, epochs: 10,
-      callbacks: {onEpochEnd: logProgress} });
+  callbacks: {onEpochEnd: logProgress} });
 
   outputsAsTensor.dispose();
   oneHotOutputs.dispose();
@@ -166,7 +164,7 @@ function predictLoop() {
     tf.tidy(function() {
       let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
       let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor,[MOBILE_NET_INPUT_HEIGHT,
-          MOBILE_NET_INPUT_WIDTH], true);
+      MOBILE_NET_INPUT_WIDTH], true);
 
       let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
       let prediction = model.predict(imageFeatures).squeeze();
@@ -206,10 +204,6 @@ function reset() {
 }
 
 
-
-
-
-
 //「ポン」(卓球ゲーム)もどき
 // マウスで両方のパドルを走査する
 
@@ -221,12 +215,9 @@ let isGameset = true;
 // ゲームの現在の状態（起動・勝敗など）にあわせて表示される画像が入る変数
 let currentImg;
 
-
 let start;
 let p1Win;
 let p2Win;
-
-
 
 let Leftp = 200;
 let Rightp = 200;
@@ -235,143 +226,131 @@ let startButton = document.getElementById("btn-start");
 let againButton = document.getElementById("btn-again");
 
 function preload() {
-    start = loadImage("images/start.jpg");
-    p1Win = loadImage("images/p1Win.jpg");
-    p2Win = loadImage("images/p2Win.jpg");
-    currentImg = start;
+  start = loadImage("images/start.jpg");
+  p1Win = loadImage("images/p1Win.jpg");
+  p2Win = loadImage("images/p2Win.jpg");
+  currentImg = start;
 }
 
 startButton.addEventListener(`click`, () => {
-    console.log('startButton がクリックされました');
-    isGameset = false;
-    // ボールは最初、キャンバス中央から左へ進む
-    ballSp.position.x = width / 2;
-    ballSp.position.y = height / 2;
-    ballSp.setSpeed(MAX_SPEED, 160);
-    p1Score = 0;
-    p2Score = 0;
+  console.log('startButton がクリックされました');
+  isGameset = false;
+  // ボールは最初、キャンバス中央から左へ進む
+  ballSp.position.x = width / 2;
+  ballSp.position.y = height / 2;
+  ballSp.setSpeed(MAX_SPEED, 160);
+  p1Score = 0;
+  p2Score = 0;
 });
 
 againButton.addEventListener(`click`, () => {
-    console.log('againButton がクリックされました');
-    currentImg = start;
-    // ボールは最初、キャンバス中央から左へ進む
-    ballSp.position.x = width / 2;
-    ballSp.position.y = height / 2;
-    ballSp.setSpeed(MAX_SPEED, 160);
-    p1Score = 0;
-    p2Score = 0;
+  console.log('againButton がクリックされました');
+  currentImg = start;
+  // ボールは最初、キャンバス中央から左へ進む
+  ballSp.position.x = width / 2;
+  ballSp.position.y = height / 2;
+  ballSp.setSpeed(MAX_SPEED, 160);
+  p1Score = 0;
+  p2Score = 0;
 });
 
-
-
-
-
 //score
- let p1Score = 0;
- let p2Score = 0;
- let page = 0;
-
-
-
+let p1Score = 0;
+let p2Score = 0;
+let page = 0;
 
 function setup() {
-    createCanvas(800, 400);
+  createCanvas(800, 400);
 
 	textSize(50);
 
-    // display = none;
-    //frameRate(6);
-    const w = 10;
-    const h = 100;
-    const offset = 30;
+  // display = none;
+  //frameRate(6);
+  const w = 10;
+  const h = 100;
+  const offset = 30;
 
-    noStroke();
-    reset=0;
+  noStroke();
+  reset=0;
 
 
    
 
 
-    // 左のパドルスプライト
-    leftPaddleSp = makeSprite(offset, height / 2, w, h, true, color(255, 0, 0));
-    // 右のパドルスプライト
-    rightPaddleSp = makeSprite(width - offset, height / 2, w, h, true, color(255, 0, 255));
-    // 上の壁スプライト
-    wallTopSp = makeSprite(width / 2, -10, width, offset, true, color(0, 255, 0));
-    // 下の壁スプライト
-    wallBottomSp = makeSprite(width / 2, height + 10, width, offset, true, color(0, 255, 0));
-    // ボールスプライト
-    ballSp = makeSprite(width / 2, height / 2, 10, 10, false, color(255));
-    // 速くなりすぎないように制限を設ける
-    ballSp.maxSpeed = MAX_SPEED;
-    // ボールは最初、キャンバス中央から左へ進む
-    ballSp.setSpeed(MAX_SPEED, -180);
+  // 左のパドルスプライト
+  leftPaddleSp = makeSprite(offset, height / 2, w, h, true, color(255, 0, 0));
+  // 右のパドルスプライト
+  rightPaddleSp = makeSprite(width - offset, height / 2, w, h, true, color(255, 0, 255));
+  // 上の壁スプライト
+  wallTopSp = makeSprite(width / 2, -10, width, offset, true, color(0, 255, 0));
+  // 下の壁スプライト
+  wallBottomSp = makeSprite(width / 2, height + 10, width, offset, true, color(0, 255, 0));
+  // ボールスプライト
+  ballSp = makeSprite(width / 2, height / 2, 10, 10, false, color(255));
+  // 速くなりすぎないように制限を設ける
+  ballSp.maxSpeed = MAX_SPEED;
+  // ボールは最初、キャンバス中央から左へ進む
+  ballSp.setSpeed(MAX_SPEED, -180);
 }
 
 // スプライトを作成し、与えられた引数でプロパティを設定したスプライトを返す
 function makeSprite(xpos, ypos, w, h, isImmovable, col) {
-    const sp = createSprite();
-    sp.width = w;
-    sp.height = h;
-    sp.position.x = xpos;
-    sp.position.y = ypos;
-    sp.immovable = isImmovable;
-    sp.shapeColor = col;
-    return sp;
+  const sp = createSprite();
+  sp.width = w;
+  sp.height = h;
+  sp.position.x = xpos;
+  sp.position.y = ypos;
+  sp.immovable = isImmovable;
+  sp.shapeColor = col;
+  return sp;
 }
 
 function draw() {
 
-  
-        if (isGameset) {
-            background(255);
-            // 画像の幅と高さは適当
-            image(currentImg, 0, 0, 800, 400);
-        } else {
-        background(0);     
-        update();
-        drawSprites();
-        fill(255);
-        text(p1Score, 200, 100);
-        text(p2Score, 550, 100);
+  if (isGameset) {
+    background(255);
+    // 画像の幅と高さは適当
+    image(currentImg, 0, 0, 800, 400);
+  } else {
+    background(0);     
+    update();
+    drawSprites();
+    fill(255);
+    text(p1Score, 200, 100);
+    text(p2Score, 550, 100);
 
-    }
+  }
    
 }
 
-    function keyPressed() {
+function keyPressed() {
       
-        if ((key == "A") || (key == "a")) {
+  if ((key == "A") || (key == "a")) {
          
-            Leftp= Leftp - 50
-        }else if((key == "Z") || (key == "z")){
-            Leftp= Leftp + 50
-        }
+    Leftp= Leftp - 50
+  }else if((key == "Z") || (key == "z")){
+    Leftp= Leftp + 50
+  }
 
-        if ((key == "k") || (key == "K")) {
+  if ((key == "k") || (key == "K")) {
          
-            Rightp= Rightp- 50
-        }else if((key == "m") || (key == "M")){
-            Rightp= Rightp + 50
-        }
+    Rightp= Rightp- 50
+  }else if((key == "m") || (key == "M")){
+    Rightp= Rightp + 50
+  }
 
-    }
+}
 
 function update() {
-
- 
-
 
   if(pose === "Class 1" && accuracy >= 85){ 
     document.body.addEventListener("keydown",
     event => {
-        if (event.key == "a" || event.key == "A") {
-           
-        }
+      if (event.key == "a" || event.key == "A") {
+        alert("Ctrl+Vが押されました")
+      }
     });
      
-  
   }else if(pose === "Class 2" && accuracy >= 85){
     
     
@@ -385,72 +364,72 @@ function update() {
   }
 
 
-    // パドルがキャンバスから出ないように、上下の動きを制限し、
-    // 右パドルを左パドルの動きに同期させる。
-    leftPaddleSp.position.y = constrain(Leftp, leftPaddleSp.height / 2, height - leftPaddleSp.height / 2);
+  // パドルがキャンバスから出ないように、上下の動きを制限し、
+  // 右パドルを左パドルの動きに同期させる。
+  leftPaddleSp.position.y = constrain(Leftp, leftPaddleSp.height / 2, height - leftPaddleSp.height / 2);
 
-    rightPaddleSp.position.y = constrain(Rightp, leftPaddleSp.height / 2, height - leftPaddleSp.height / 2);
-
-
+  rightPaddleSp.position.y = constrain(Rightp, leftPaddleSp.height / 2, height - leftPaddleSp.height / 2);
 
 
-    // ボールは上の壁に当たったら跳ね返る
-    ballSp.bounce(wallTopSp);
-    // ボールは下の壁に当たったら跳ね返る
-    ballSp.bounce(wallBottomSp);
 
-    // 入射角＝反射角とする => ballSp.getDirection()
-    // 「反射の法則」 https://exam.fukuumedia.com/rika1-13/#i-3
-    // ただし、ボールの芯とパドルの芯のずれが大きいと、反射角も大きくなる
 
-    // ボールが左パドルに当たったら
-    if (ballSp.bounce(leftPaddleSp)) {
-        // ボールの芯とパドルの芯のずれ。
-        const swing = (ballSp.position.y - leftPaddleSp.position.y) / 3;
-        // 左パドルの場合、角度は時計回りに大きくなるので、角度を大きくするにはswingを足す
-        ballSp.setSpeed(MAX_SPEED, ballSp.getDirection() + swing);
-        print(ballSp.getDirection())
-    }
+  // ボールは上の壁に当たったら跳ね返る
+  ballSp.bounce(wallTopSp);
+  // ボールは下の壁に当たったら跳ね返る
+  ballSp.bounce(wallBottomSp);
 
-    // ボールが右パドルに当たったら
-    if (ballSp.bounce(rightPaddleSp)) {
-        const swing = (ballSp.position.y - rightPaddleSp.position.y) / 3;
-        // 右パドルの場合、角度は反時計回りに大きくなるので、角度を大きくするにはswingを引く
-        ballSp.setSpeed(MAX_SPEED, ballSp.getDirection() - swing);
-    }
+  // 入射角＝反射角とする => ballSp.getDirection()
+  // 「反射の法則」 https://exam.fukuumedia.com/rika1-13/#i-3
+  // ただし、ボールの芯とパドルの芯のずれが大きいと、反射角も大きくなる
+
+  // ボールが左パドルに当たったら
+  if (ballSp.bounce(leftPaddleSp)) {
+    // ボールの芯とパドルの芯のずれ。
+    const swing = (ballSp.position.y - leftPaddleSp.position.y) / 3;
+    // 左パドルの場合、角度は時計回りに大きくなるので、角度を大きくするにはswingを足す
+    ballSp.setSpeed(MAX_SPEED, ballSp.getDirection() + swing);
+    print(ballSp.getDirection())
+  }
+
+  // ボールが右パドルに当たったら
+  if (ballSp.bounce(rightPaddleSp)) {
+    const swing = (ballSp.position.y - rightPaddleSp.position.y) / 3;
+    // 右パドルの場合、角度は反時計回りに大きくなるので、角度を大きくするにはswingを引く
+    ballSp.setSpeed(MAX_SPEED, ballSp.getDirection() - swing);
+  }
 
 
     
-    // ボールがキャンバス左端から外に出たら、真ん中に再配置し右へ動く
-    if (ballSp.position.x < 0) {
-        ballSp.position.x = width / 2;
-        ballSp.position.y = height / 2;
-        ballSp.setSpeed(MAX_SPEED, 0);
-        p2Score = p2Score + 1;
-    }
-    // ボールがキャンバス右端から外に出たら、真ん中に再配置し左へ動く
-    if (ballSp.position.x > width) {
-        ballSp.position.x = width / 2;
-        ballSp.position.y = height / 2;
-        ballSp.setSpeed(MAX_SPEED, 180);
-        p1Score = p1Score + 1;
-    }
+  // ボールがキャンバス左端から外に出たら、真ん中に再配置し右へ動く
+  if (ballSp.position.x < 0) {
+    ballSp.position.x = width / 2;
+    ballSp.position.y = height / 2;
+    ballSp.setSpeed(MAX_SPEED, 0);
+    p2Score = p2Score + 1;
+  }
+  // ボールがキャンバス右端から外に出たら、真ん中に再配置し左へ動く
+  if (ballSp.position.x > width) {
+    ballSp.position.x = width / 2;
+    ballSp.position.y = height / 2;
+    ballSp.setSpeed(MAX_SPEED, 180);
+    p1Score = p1Score + 1;
+  }
 	//check for winner
 
-    if (p1Score === 5) {
-        ballSp.setSpeed(0, 0);
-        currentImg = p1Win;
-        isGameset = true;
-        p1Score = 0;
-        p2Score = 0;
+  if (p1Score === 5) {
+    ballSp.setSpeed(0, 0);
+    currentImg = p1Win;
+    isGameset = true;
+    p1Score = 0;
+    p2Score = 0;
 	}
 
-    if (p2Score === 5) {
-        ballSp.setSpeed(0, 0);
-        currentImg = p2Win;
-        isGameset = true;
-        p1Score = 0;
-        p2Score = 0;
+  if (p2Score === 5) {
+    ballSp.setSpeed(0, 0);
+    currentImg = p2Win;
+    isGameset = true;
+    p1Score = 0;
+    p2Score = 0;
 	}
    
 
