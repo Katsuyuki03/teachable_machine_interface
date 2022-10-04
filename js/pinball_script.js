20d0cc73140e24ae125eea1bf5bd5e73446ca0ff
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-10-04 20:39:49 +0900

    プログレスバーを入れてみた
    大きさの調整は未完成。

--- js/pinball_script.js
+++ js/pinball_script.js
@@ -232,7 +232,16 @@ function preload() {
   currentImg = start;
 }
 
-
+const bar = new ProgressBar.Line(container, {
+  strokeWidth: 1,
+  easing: "easeInOut",
+  duration: 1400,
+  color: "#FFEA82",
+  trailColor: "#eee",
+  trailWidth: 1,
+  svgStyle: {width: "100%", height: "100%"}
+});
+bar.animate(1.0);
 
 // let processor = {
 //   timerCallback: function() {
