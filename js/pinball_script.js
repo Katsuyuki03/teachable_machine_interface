5d6f9421e04a48f6d389240c6f473ed2b70a7991
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-10-04 20:18:13 +0900

    画面の位置を整理した
    より見やすい画面位置に変えた

--- js/pinball_script.js
+++ js/pinball_script.js
@@ -232,52 +232,54 @@ function preload() {
   currentImg = start;
 }
 
-let processor = {
-  timerCallback: function() {
-    if (this.video.paused || this.video.ended) {
-      return;
-    }
-    this.computeFrame();
-    let self = this;
-    setTimeout(function () {
-        self.timerCallback();
-      }, 0);
-  },
 
-  doLoad: function() {
-    this.video = document.getElementById("webcam");
-    this.c1 = document.getElementById("c1");
-    this.ctx1 = this.c1.getContext("2d");
-    // this.c2 = document.getElementById("c2");
-    // this.ctx2 = this.c2.getContext("2d");
-    let self = this;
-    this.video.addEventListener("play", function() {
-        self.width = self.video.videoWidth / 2;
-        self.height = self.video.videoHeight / 2;
-        self.timerCallback();
-      }, false);
-  },
 
-  computeFrame: function() {
-    this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-    let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
-        let l = frame.data.length / 4;
+// let processor = {
+//   timerCallback: function() {
+//     if (this.video.paused || this.video.ended) {
+//       return;
+//     }
+//     this.computeFrame();
+//     let self = this;
+//     setTimeout(function () {
+//         self.timerCallback();
+//       }, 0);
+//   },
 
-    for (let i = 0; i < l; i++) {
-      let r = frame.data[i * 4 + 0];
-      let g = frame.data[i * 4 + 1];
-      let b = frame.data[i * 4 + 2];
-      if (g > 100 && r > 100 && b < 43)
-        frame.data[i * 4 + 3] = 0;
-    }
-    this.ctx2.putImageData(frame, 0, 0);
-    return;
-  }
-};
+//   doLoad: function() {
+//     this.video = document.getElementById("webcam");
+//     this.c1 = document.getElementById("c1");
+//     this.ctx1 = this.c1.getContext("2d");
+//     // this.c2 = document.getElementById("c2");
+//     // this.ctx2 = this.c2.getContext("2d");
+//     let self = this;
+//     this.video.addEventListener("play", function() {
+//         self.width = self.video.videoWidth / 2;
+//         self.height = self.video.videoHeight / 2;
+//         self.timerCallback();
+//       }, false);
+//   },
 
-document.addEventListener("DOMContentLoaded", () => {
-processor.doLoad();
-});
+//   computeFrame: function() {
+//     this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+//     let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+//         let l = frame.data.length / 4;
+
+//     for (let i = 0; i < l; i++) {
+//       let r = frame.data[i * 4 + 0];
+//       let g = frame.data[i * 4 + 1];
+//       let b = frame.data[i * 4 + 2];
+//       if (g > 100 && r > 100 && b < 43)
+//         frame.data[i * 4 + 3] = 0;
+//     }
+//     this.ctx2.putImageData(frame, 0, 0);
+//     return;
+//   }
+// };
+
+// document.addEventListener("DOMContentLoaded", () => {
+// processor.doLoad();
+// });
 
 startButton.addEventListener(`click`, () => {
   console.log('startButton がクリックされました');
@@ -307,7 +309,12 @@ let p2Score = 0;
 let page = 0;
 
 function setup() {
-  createCanvas(800, 400);
+  const canvas = createCanvas( 800, 400 );
+  
+  // HTMLのdiv game-screen に canvasを挿入
+  canvas.parent("game-screen");
+
+  // createCanvas(800, 400);
 
 	textSize(50);
 
