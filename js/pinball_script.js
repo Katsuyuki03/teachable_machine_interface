8dd8e8c15069deffaad118da419baf40cdce7bc4
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-10-04 14:52:29 +0900

    videoのコピーをcanvasに映そうとした。(失敗)
    videoのコピーをcanvasに映そうとした。(失敗)

--- js/pinball_script.js
+++ js/pinball_script.js
@@ -232,6 +232,53 @@ function preload() {
   currentImg = start;
 }
 
+let processor = {
+  timerCallback: function() {
+    if (this.video.paused || this.video.ended) {
+      return;
+    }
+    this.computeFrame();
+    let self = this;
+    setTimeout(function () {
+        self.timerCallback();
+      }, 0);
+  },
+
+  doLoad: function() {
+    this.video = document.getElementById("webcam");
+    this.c1 = document.getElementById("c1");
+    this.ctx1 = this.c1.getContext("2d");
+    // this.c2 = document.getElementById("c2");
+    // this.ctx2 = this.c2.getContext("2d");
+    let self = this;
+    this.video.addEventListener("play", function() {
+        self.width = self.video.videoWidth / 2;
+        self.height = self.video.videoHeight / 2;
+        self.timerCallback();
+      }, false);
+  },
+
+  computeFrame: function() {
+    this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+    let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+        let l = frame.data.length / 4;
+
+    for (let i = 0; i < l; i++) {
+      let r = frame.data[i * 4 + 0];
+      let g = frame.data[i * 4 + 1];
+      let b = frame.data[i * 4 + 2];
+      if (g > 100 && r > 100 && b < 43)
+        frame.data[i * 4 + 3] = 0;
+    }
+    this.ctx2.putImageData(frame, 0, 0);
+    return;
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+processor.doLoad();
+});
+
 startButton.addEventListener(`click`, () => {
   console.log('startButton がクリックされました');
   isGameset = false;
@@ -344,26 +391,19 @@ function keyPressed() {
 function update() {
 
   if(pose === "Class 1" && accuracy >= 85){ 
-    document.body.addEventListener("keydown",
-    event => {
-      if (event.key == "a" || event.key == "A") {
-        alert("Ctrl+Vが押されました")
-      }
-    });
-     
+    Leftp= Leftp - 10
   }else if(pose === "Class 2" && accuracy >= 85){
-    
-    
+    Leftp= Leftp + 10
   }else if(pose === "Class 3" && accuracy >= 85){ 
-     
+    Rightp= Rightp - 10
   }else if(pose === "Class 4" && accuracy >= 85){
-    
+    Rightp= Rightp+ 10
   }else{
     direction = 0;
     vertical = 0;
   }
 
-
+  console.log(pose);
   // パドルがキャンバスから出ないように、上下の動きを制限し、
   // 右パドルを左パドルの動きに同期させる。
   leftPaddleSp.position.y = constrain(Leftp, leftPaddleSp.height / 2, height - leftPaddleSp.height / 2);
