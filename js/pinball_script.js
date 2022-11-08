c15382a2801312fb0a7c4fae1af9f52f1b4cae6b
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-11-08 19:41:20 +0900

    バーのスピードを変えた
    バーのスピードを変えた

--- js/pinball_script.js
+++ js/pinball_script.js
@@ -585,22 +585,24 @@ function keyPressed() {
 function update() {
 
   if(pose[0] === "Class 1" && accuracy >= 85){ 
-    Leftp= Leftp - 10
+    Leftp= Leftp - 6
   }else if(pose[0] === "Class 2" && accuracy >= 85){
-    Leftp= Leftp + 10
+    Leftp= Leftp + 6
   }else {
     direction = 0;
     vertical = 0;
   }
   
   if(pose[1] === "Class 3" && accuracy >= 85){ 
-    Rightp= Rightp - 10
+    Rightp= Rightp - 6
   }else if(pose[1] === "Class 4" && accuracy >= 85){
-    Rightp= Rightp + 10
+    Rightp= Rightp + 6
   }else{
     direction = 0;
     vertical = 0;
   }
+
+
 
   console.log(pose);
   // パドルがキャンバスから出ないように、上下の動きを制限し、
