c83a91d31f610678ca14db294ee0fda858aa2363
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-11-18 14:32:58 +0900

    鬼滅をボボボーボ・ボーボボに変える拡張機能が完成
    version2と3の書き方が違かった

--- Chrome-rensyu/popup.js
+++ Chrome-rensyu/popup.js
@@ -3,9 +3,11 @@ let buttonElement = document.getElementById('button');
 
 buttonElement.addEventListener('click', (event) => {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: `document.body.innerHTML=document.body.innerHTML.replace(/鬼滅の刃/g, 'ボボボーボ・ボーボボ');`}
-        );
+        chrome.scripting.executeScript({
+          target: {
+            tabId: tabs[0].id,
+          },
+          files: ['script.js'],
+        });
     }); 
 });
