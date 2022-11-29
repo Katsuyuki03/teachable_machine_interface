69d48217a286610a04ac5f588e0bbf9f20294af7
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-11-29 21:47:39 +0900

    popup.jsのボタンを押すとcontent.jsが動くようにした
    popup.jsのボタンを押すとcontent.jsが動くようにした

--- youtube-extension/content.js
+++ youtube-extension/content.js
@@ -1,10 +1,32 @@
-// $(function(){
-//     $("button").click(function(){
-//       const $playButton = document.getElementsByClassName("ytp-play-button ytp-button")[0];
-//       if( $playButton ) $playButton.click();
-//     //For debugging
-//     });
-//   });
+    // $(function(){
+    //     $("button").click(function(){
+    //     const $playButton = document.getElementsByClassName("ytp-play-button ytp-button")[0];
+    //     if( $playButton ) $playButton.click();
+    //     //For debugging
+    //     });
+    // });
+
+// popupからのメッセージを受け取る
+
+    chrome.runtime.onMessage.addListener((send_data, sender, callback) => {
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const entryElement = document.getElementById("btn");
+            entryElement.addEventListener('click', function() {
+                         //何かしらの処理
+            });
+        });
+    //何かしらの処理
+    const ai = document.querySelector("#movie_player > div.html5-video-container > video");
+
+    // コールバック関数の実行
+    callback(button);
+  
+    // おまじないで必要らしい
+    return true;
+  });
 
 
-console.log("Hello");
+  
+//   console.log();
+//   setTimeout(() => {ai.click()}, 3000);
--- youtube-extension/popup.js
+++ youtube-extension/popup.js
@@ -1,6 +1,23 @@
 
-let buttonElement = document.getElementById('button');
+// contentにメッセージを送る
+chrome.tabs.query({ active: true, currentWindow: true }, aim_message);
+
+let buttonElement = document.getElementById('btn');
 
 buttonElement.addEventListener('click', (event) => {
-  
+    const sendMessageId = document.getElementById("sendmessageid");
+    if (sendMessageId) {
+    sendMessageId.onclick = function() {
+    setTimeout(() => {ai.click()}, 3000);
+  };
+}
 });
+
+// function aim_message(tabs) {
+//   // 今開いているタブに、データを渡す（send_data, callbackは省略可）
+//   chrome.tabs.sendMessage(tabs[0].id, send_data, callback);
+// }
+
+// function callback(callback_data) {
+//   // コールバック関数の処理（popupからの値をlocalstorageに保存したり）
+// }
