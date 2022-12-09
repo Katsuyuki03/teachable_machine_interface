62d22e7eb4a581dffd80472f68271fc39e9ce62c
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-12-09 15:48:37 +0900

    popup.jsでcontent.jsを動かせた。
    popup.jsでcontent.jsを動かせた。

--- youtube-extension/content.js
+++ youtube-extension/content.js
@@ -8,25 +8,18 @@
 
 // popupからのメッセージを受け取る
 
-    chrome.runtime.onMessage.addListener((send_data, sender, callback) => {
 
-        document.addEventListener('DOMContentLoaded', function() {
-            const entryElement = document.getElementById("btn");
-            entryElement.addEventListener('click', function() {
-                         //何かしらの処理
-            });
-        });
-    //何かしらの処理
-    const ai = document.querySelector("#movie_player > div.html5-video-container > video");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let title = document.querySelector("#movie_player > div.html5-video-container > video");
+    sendResponse("content.js からのメッセージだよー");
+    title.click(function() {
+      });
 
-    // コールバック関数の実行
-    callback(button);
-  
-    // おまじないで必要らしい
-    return true;
-  });
+});
+
+    
 
 
-  
+// setTimeout(() => {title.click()}, 3000);
 //   console.log();
 //   setTimeout(() => {ai.click()}, 3000);
--- youtube-extension/popup.js
+++ youtube-extension/popup.js
@@ -1,23 +1,18 @@
 
 // contentにメッセージを送る
-chrome.tabs.query({ active: true, currentWindow: true }, aim_message);
 
-let buttonElement = document.getElementById('btn');
 
-buttonElement.addEventListener('click', (event) => {
-    const sendMessageId = document.getElementById("sendmessageid");
-    if (sendMessageId) {
-    sendMessageId.onclick = function() {
-    setTimeout(() => {ai.click()}, 3000);
-  };
-}
+document.getElementById("btn").addEventListener("click", (event) => {
+  chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
+    console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id, {message: 'getname'}, (content) => {
+        if(!content){
+            alert('Cannot Get! Try Reload First!');
+            return;
+        }
+        document.getElementById('title').value = "受け取ったメッセージは　"+content
+    });
+});
+  
 });
 
-// function aim_message(tabs) {
-//   // 今開いているタブに、データを渡す（send_data, callbackは省略可）
-//   chrome.tabs.sendMessage(tabs[0].id, send_data, callback);
-// }
-
-// function callback(callback_data) {
-//   // コールバック関数の処理（popupからの値をlocalstorageに保存したり）
-// }
