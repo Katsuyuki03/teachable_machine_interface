a1d367e5f442b32aa4e439343c88b54c3e405779
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-12-13 21:27:04 +0900

    content_security_policyをやっていた
    content_security_policyをやっていた

--- youtube-extension/content.js
+++ youtube-extension/content.js
@@ -13,11 +13,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     let title = document.querySelector("#movie_player > div.html5-video-container > video");
     sendResponse("content.js からのメッセージだよー");
     title.click(function() {
-      });
+
+    });
 
 });
 
-    
+
 
 
 // setTimeout(() => {title.click()}, 3000);
