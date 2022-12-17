150b634c3f9039ceb1928eccf492d344ccc85b25
Katsuyuki03 <katsuyukid.1412@gmail.com>
2022-12-18 03:04:09 +0900

    わかりずらくなるからファイルを書き直した
    わかりずらくなるからファイルを書き直した

--- bobobo-extention/popup.js
+++ bobobo-extention/popup.js
@@ -1,4 +1,3 @@
-
 let buttonElement = document.getElementById('button');
 
 buttonElement.addEventListener('click', (event) => {
--- bobobo-extention/script.js
+++ bobobo-extention/script.js
@@ -2,4 +2,3 @@ document.body.innerHTML = document.body.innerHTML.replace(
     /鬼滅の刃/g,
     'ボボボーボ・ボーボボ'
   );
-  
