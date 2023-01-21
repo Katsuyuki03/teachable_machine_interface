91395ec92a2393ce88422771f61c35cea10bf40d
Katsuyuki03 <katsuyukid.1412@gmail.com>
2023-01-21 15:07:32 +0900

    プログレスバーをforEachでまとめようとした(失敗)
    プログレスバーをforEachでまとめようとした(失敗)

--- mitecon/popup/main.js
+++ mitecon/popup/main.js
@@ -9,17 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
   let learningStop = document.getElementById('learning-stop');
   let opacityRange = document.getElementById('opacity-range');
 
-  const barUp = document.querySelectorAll('.up-progress-bar');
-  const barDown = document.querySelectorAll('.down-progress-bar');
-  const barplay = document.querySelectorAll('.play-progress-bar');
-  const barnoisy = document.querySelectorAll('.noisy-progress-bar');
-  const barsilent = document.querySelectorAll('.silent-progress-bar');
-  const barscreen = document.querySelectorAll('.screen-progress-bar');
-  const barStop = document.querySelectorAll('.stop-progress-bar');
+  // const barUp = document.querySelectorAll('.up-progress-bar');
+  // const barDown = document.querySelectorAll('.down-progress-bar');
+  // const barplay = document.querySelectorAll('.play-progress-bar');
+  // const barnoisy = document.querySelectorAll('.noisy-progress-bar');
+  // const barsilent = document.querySelectorAll('.silent-progress-bar');
+  // const barscreen = document.querySelectorAll('.screen-progress-bar');
+  // const barStop = document.querySelectorAll('.stop-progress-bar');
 
 
-  // const bar = document.querySelectorAll('.js-progress-bar');
-  const number = [20,50,50,50,50,50,50];
+  const bar = document.querySelectorAll('.progress-bar');
 
   modeImage.addEventListener('change', () => {
     // backgroundには遅れるがcontentsの方には送れない
@@ -31,41 +30,66 @@ document.addEventListener('DOMContentLoaded', () => {
     chromTabsQueryMode("image", modeImage.checked);
   });
   
-  learningUp.addEventListener('click', () => {
-    number[1] = number[1] + 30;
-    barUp[0].style.width = Math.min( number[1]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("up");
-  });
-  learningDown.addEventListener('click', () => {
-    number[2] = number[2] + 30;
-    barDown[0].style.width = Math.min( number[2]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("down");
-  });
-  learningplay.addEventListener('click', () => {
-    number[3] = number[3] + 30;
-    barplay[0].style.width = Math.min( number[3]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("play");
-  });
-  learningnoisy.addEventListener('click', () => {
-    number[4] = number[4] + 30;
-    barnoisy[0].style.width = Math.min( number[4]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("noisy");
-  });
-  learningsilent.addEventListener('click', () => {
-    number[5] = number[5] + 30;
-    barsilent[0].style.width = Math.min( number[5]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("silent");
-  });
-  learningscreen.addEventListener('click', () => {
-    number[6] = number[6] + 30;
-    barscreen[0].style.width = Math.min( number[6]*0.5 , 100) + "%" ;
-    chromTabsQueryArrow("screen");
-  });
-  learningStop.addEventListener('click', () => {
-    number[0] = number[0] + 10;
-    barStop[0].style.width = Math.min( 0 + number[0], 100) + "%" ;
-    chromTabsQueryArrow("stop");
-  });
+    learningUp.addEventListener('click', () => {
+      chromTabsQueryArrow("up");
+    });
+    learningDown.addEventListener('click', () => {
+      chromTabsQueryArrow("down");
+    });
+    learningplay.addEventListener('click', () => {
+      chromTabsQueryArrow("play");
+    });
+    learningnoisy.addEventListener('click', () => {
+      chromTabsQueryArrow("noisy");
+    });
+    learningsilent.addEventListener('click', () => {
+      chromTabsQueryArrow("silent");
+    });
+    learningscreen.addEventListener('click', () => {
+      chromTabsQueryArrow("screen");
+    });
+    learningStop.addEventListener('click', () => {
+      chromTabsQueryArrow("stop");
+    });
+
+    bar.forEach(function(learningUp, learningDown,learningplay,learningnoisy,learningsilent,
+    learningscreen, learningStop,index) {
+      learningUp.addEventListener('click', () => {
+      
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningDown.addEventListener('click', () => {
+      
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningplay.addEventListener('click', () => {
+        
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningnoisy.addEventListener('click', () => {
+        
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningsilent.addEventListener('click', () => {
+        
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningscreen.addEventListener('click', () => {
+        
+        bar[index].style.width = Math.min( bar[index]+10 , 100) + "%" ;
+      });
+      learningStop.addEventListener('click', () => {
+
+        bar[index].style.width = Math.min( bar[index]+5 , 100) + "%" ;
+      });
+    });
+
+
+
+
+
+
+
   opacityRange.addEventListener('change', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
@@ -74,6 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
   });
+
 
 
   function chromTabsQueryMode (mode, checked) {
